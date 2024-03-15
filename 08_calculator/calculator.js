08704c125680631c9610db4f1e8cde79ba36f289
fugbot <18075133+fugbot@users.noreply.github.com>
2024-03-15 14:55:48 +0000

    Add functions for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((acc, currentValue) => acc + currentValue, 0) 
+};
+
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current, 1)
+
+};
+
+const power = function(a,b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for (i=1; i<=n; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
