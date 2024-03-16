78f22f124ba4464ee42ea6863f61dffdd411b27f
fugbot <18075133+fugbot@users.noreply.github.com>
2024-03-16 10:36:14 +0000

    Add code for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    if(typeof pos == "string"){
+        pos = Number(pos);
+    }
+    if(pos<0){
+        return "OOPS";
+    }
+    if(pos === 0){
+        return 0;
+    }
 
+    let array = [];
+    let num = 1;
+    for (let i = 1; i <= pos; i++){
+        if(array.length < 2){
+            array.push(1);
+            continue;
+        }
+        num = array[array.length-2] + array[array.length-1];
+        array.push(num);
+        
+    } 
+    
+    console.log(array);
+    return array[array.length-1];
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
