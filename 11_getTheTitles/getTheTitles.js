51e422333496706e5cb0b1b796677b38b96539a8
fugbot <18075133+fugbot@users.noreply.github.com>
2024-03-16 10:50:44 +0000

    Add code for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function(arr) {
+    // let titleArray = [];
+    // arr.filter(function(obj) { 
+        
+    //     console.log(obj.title) 
+    //     titleArray.push(obj.title);
+        
+    // });
+    // return titleArray;
+
+    return arr.map(book => book.title);
 };
 
 // Do not edit below this line
